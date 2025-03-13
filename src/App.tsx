0786d331138c48557f87a6716c98a5f4363209dd
ushakoviii <ushakov.ii@yandex.ru>
2025-03-13 23:49:26 +0300

    Commit_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import {theme} from "./components/styles/Theme";
+import {Card} from "./components/Card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <StyledBody>
+            <Card/>
+        </StyledBody>
+    );
 }
 
+const StyledBody = styled.body`
+  background-color: ${theme.colors.backgroundColor};
+  height: 100vh;
+  display: flex;
+  justify-content: center; 
+  align-items: center;  
+  font-family: Arial;
+`
 export default App;
